e497853085d7d8d441d6576dc3ecdb1e924cfbe7
Jingya <43267493+jingya221@users.noreply.github.com>
2020-08-25 19:53:52 -0400

    Update and rename README.md to README.r

---
+++ README.r
@@ -0,0 +1,9 @@
+###################################################
+#### Guide for using the interact learning exercise
+###################################################
+#install.package("devtools")
+#install.package("learnr")
+devtools::install_github("jingya221/testlearnr/rfds")
+learnr::run_tutorial("Lesson1", package = "rfds")
+
+##for further use, "Lesson1" can be changed with the course
