f606eabdf44d1d2d61ae868826f9a92f1f517025
Jingya <43267493+jingya221@users.noreply.github.com>
2020-09-08 19:37:54 -0400

    Update README.r

--- README.r
+++ README.r
@@ -3,7 +3,15 @@
 ###################################################
 #install.package("devtools")
 #install.package("learnr")
+
+## Run this code each time you use the lesson tutorails
 devtools::install_github("jingya221/introRprogramming/rfds")
+
+##lesson1
 learnr::run_tutorial("Lesson1", package = "rfds")
+
+##lesson2
 learnr::run_tutorial("Lesson2", package = "rfds")
-##for further use, "Lesson1" can be changed with the course
+
+##lesson3
+learnr::run_tutorial("Lesson3", package = "rfds")
